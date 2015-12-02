348f9106adc9c1105bdccfd2f5771aa655dc329b
machinefixer <waywardson.wayne@gmail.com>
2015-12-02 15:33:02 +0800

    Initial Commit

---
+++ SearchBarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchBarDemo
+//
+//  Created by Anderson on 15/12/2.
+//  Copyright © 2015年 Yuchen Zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
